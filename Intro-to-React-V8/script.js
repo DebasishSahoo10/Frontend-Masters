21029c6e1fb924ab372e04fd1344e45619ccc024
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-04-05 21:02:20 +0530

    react : created new Pet component

--- Intro-to-React-V8/script.js
+++ Intro-to-React-V8/script.js
@@ -1,9 +1,26 @@
 // our JavaScript goes here
+const Pet = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1", {}, "Purple"),
+            React.createElement("h2", {}, "Labrador"),
+            React.createElement("h2", {}, "4 Months")
+        ]
+    )
+}
+
+
 const App = () => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1", {}, "Adopt Me App")
+        [
+            React.createElement("h1", {}, "Adopt Me App"),
+            React.createElement(Pet),
+            React.createElement(Pet)
+        ]
     )
 } 
 
