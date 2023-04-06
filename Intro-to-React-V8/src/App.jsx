b4b0913ab82c7237d878c8dc2b74b754c09704d8
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-04-06 13:14:41 +0530

    react : added useState hook

--- Intro-to-React-V8/src/App.jsx
+++ Intro-to-React-V8/src/App.jsx
@@ -1,15 +1,16 @@
-import Pet from './Pet'
-import { SearchParams } from './SearchParams';
+import { createRoot } from "react-dom/client";
+import Pet from "./Pet";
+import { SearchParams } from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me App</h1>
-      <Pet name="Purple" breed="Labrador" age="4 Months"/>
-      <Pet name="Coco" breed="GSD" age="3 Years"/>
-      <SearchParams/>
+      <Pet name="Purple" breed="Labrador" age="4 Months" />
+      <Pet name="Coco" breed="GSD" age="3 Years" />
+      <SearchParams />
     </div>
-  )
+  );
 };
 
 const container = document.getElementById("root");
